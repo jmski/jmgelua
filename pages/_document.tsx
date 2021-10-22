3fcf91957e4c6d5ef25b4242fe84692c08530072
jmski <geluajm@gmail.com>
2021-10-21 22:42:54 -0400

    Made about me and project section

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+        <meta name="description" content="Jon-Michael Gelua Portfolio" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+            rel="preload"
+            href="/fonts/Lexend/Lexend-Bold.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/Lexend/Lexend-Regular.ttf"
+            as="font"
+            crossOrigin=""
+          />
+        </Head>
         <body className=" bg-gray-200">
           <Main />
           <NextScript />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Lexend", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {
       backgroundImage: ['hover', 'focus'],
+      fontFamily: ['hover', 'focus'],
     },
+  },
+  options: {
+    fontFace: true,
   },
   plugins: [],
 }
