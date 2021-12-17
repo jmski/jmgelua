0ea879262e19d549f980981db051a60563804576
jmski <geluajm@gmail.com>
2021-12-17 10:57:22 -0500

    Add v2 navbar
    moved files into v1
    create test route

---
+++ pages/test.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/v2/NavBar";
+
+const Test = () => {
+  return (
+    <>
+      <NavBar />
+      <h1>Hello world</h1>
+    </>
+  );
+};
+
+export default Test;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,42 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    fontFamily: {
+      sans: ["Lexend", ...defaultTheme.fontFamily.sans],
+    },
     extend: {
-      fontFamily: {
-        sans: ["Lexend", ...defaultTheme.fontFamily.sans],
+      lineHeight: {
+        "extra-loose": "2.5",
+        12: "3rem",
+        14: "3.5rem",
+        16: "4rem",
+      },
+      maxWidth: {
+        "8xl": "100rem",
+      },
+      minWidth: {
+        xxs: "16rem",
+        xs: "20rem",
+        sm: "24rem",
+        md: "28rem",
+        lg: "32rem",
+        xl: "36rem",
       },
     },
   },
   variants: {
     extend: {
-      backgroundImage: ['hover', 'focus'],
-      fontFamily: ['hover', 'focus'],
+      backgroundImage: ["hover", "focus"],
+      fontFamily: ["hover", "focus"],
     },
   },
   options: {
     fontFace: true,
   },
   plugins: [],
-}
+};
